eb90dfc4b72e064838160c18783591b638380315
Mark Tyrkba <awfulmarco@gmail.com>
2025-01-04 20:46:21 +0300

    Add explanation to `paths.rs` and format it a bit

--- backend/paths.rs
+++ backend/paths.rs
@@ -1,3 +1,7 @@
+/*
+    I created this module so I can import it both in `build.rs`, which is executed at pre-compile stage, and `main.rs` to import the generated certificates if needed.
+*/
+
 macro_rules! define_key_cert {
     (
         const CERTS_DIR: &str = $certs_dir: literal;
@@ -5,13 +9,13 @@ macro_rules! define_key_cert {
         const CERT_FILE: $cert_ty: ty = $cert_path: literal;
     ) => {
         #[allow(unused)] pub(crate) const CERTS_DIR: &str = $certs_dir;
-        #[allow(unused)] pub(crate) const KEY_FILE: $key_ty = concat!($certs_dir, $key_path);
+        #[allow(unused)] pub(crate) const KEY_FILE:  $key_ty = concat!($certs_dir, $key_path);
         #[allow(unused)] pub(crate) const CERT_FILE: $cert_ty = concat!($certs_dir, $cert_path);
     };
 }
 
 define_key_cert! {
     const CERTS_DIR: &str = "./certs/";
-    const KEY_FILE: &str = "key.pem";
+    const KEY_FILE:  &str = "key.pem";
     const CERT_FILE: &str = "cert.pem";
 }
